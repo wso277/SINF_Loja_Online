5329e3d846f3b176315c0ad7db95daf88f3f349e
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2014-11-14 12:58:53 +0000

    Added remaining libraries

--- app/server/app.js
+++ app/server/app.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+
 var app = express();
+
+app.set("views", __dirname + '/../views');
+app.set('view engine', 'ejs');
 
 app.listen(8080, "127.0.0.1", function () {
         console.log("Listening");
