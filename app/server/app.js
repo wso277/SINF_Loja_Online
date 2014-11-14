5c7288b9911870daacd4d2d39b1c753472b0527c
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2014-11-14 13:09:12 +0000

    Server up and running

--- app/server/app.js
+++ app/server/app.js
@@ -2,13 +2,28 @@ var express = require('express');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var path = require('path');
+var routes = require('./routes.js');
 
 var app = express();
+var port = 8080;
+var ip = "127.0.0.1";
 
-app.set("views", __dirname + '/../views');
+app.set("views", __dirname + '../views');
 app.set('view engine', 'ejs');
+app.use("/css", express.static(path.join(__dirname, '../css')));
+app.use("/images", express.static(path.join(__dirname, '../images')));
+app.use("/js", express.static(path.join(__dirname, '/../js')));
 
-app.listen(8080, "127.0.0.1", function () {
+app.use(cookieParser('shhhh, very very very secretzzzzz'));
+app.use(session({
+    secret: 'shhhh, very very very secretzzzzz',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//routes.listen(app);
+
+app.listen(port, ip, function () {
         console.log("Listening");
 
     }
---
+++ app/server/routes.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+exports.listen = function (app) {
+
+    app.get('/logout', function (req, res) {
+        var messages = generateMessageBlock();
+        // destroy the user's session to log them out
+        // will be re-created next request
+        if (req.session.user) {
+            req.session.destroy(function () {
+                messages.success.push({title: "Logged Out", content: "You are now logged out!"});
+                res.render("landing.ejs", {messages: messages, title: 'Landing'});
+            });
+        } else {
+            messages.success.push({title: "Sign in first", content: "You are not logged in"});
+            res.render("landing.ejs", {messages: messages, title: 'Landing'});
+        }
+    });
+};
