0d4bc615ff310daa7a0ba4eb1d71c2161a137f34
Miguel <ei11143@fe.up.pt>
2014-11-27 23:10:32 +0000

    Started some API files: artigos e clientes

---
+++ API/MvcApplication1/Lib_Primavera/Models/Artigo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Lib_Primavera.Models
+{
+    public class Artigo
+    {
+        public string CodigoArtigo { get; set; }
+        public string Nome { get; set; }
+        public float StockAtual { get; set; }
+
+
+        /** Preço **/
+        public float PVP { get; set; }
+        public float Desconto { get; set; }
+
+        /* Especificacoes do joao */
+         //todo
+
+
+
+    }
+}
---
+++ API/MvcApplication1/Lib_Primavera/Models/Resposta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Lib_Primavera.Models
+{
+    public class Resposta
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
