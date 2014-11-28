2fed143dfcefbaf13cb98ddb231194cdbe68c66a
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2014-11-28 12:42:42 +0000

    Pagina dos produtos feita, encomendas a caminho

---
+++ app/js/app-login.js
@@ -0,0 +1,38 @@
+(function () {
+    var app = angular.module('login-app', []);
+
+    /**
+     * Controller that manages the login information of an user
+     */
+    app.controller('LoginController', ['$scope', '$http', '$window', 'Login', function ($scope, $http, $window, Login) {
+
+        $scope.formData = {};
+        $scope.loading = true;
+
+        $scope.login = function () {
+            $scope.loading = true;
+            if ($scope.formData != undefined) {
+                Login.create($scope.formData)
+                    .success(function () {
+                        $scope.loading = false;
+                        $scope.formData = {};
+                        $window.location.href = '/';
+                    })
+                    .error(function (data) {
+                        $window.location.href = '/teste-erro';
+                    });
+            }
+        };
+    }]);
+
+    /**
+     * Service that attempts to login with provided information
+     */
+    app.factory('Login', ['$http', function ($http) {
+        return{
+            create: function (user) {
+                return $http.post('/login', user);
+            }
+        }
+    }]);
+})();
