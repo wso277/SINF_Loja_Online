b7f1814a22934922b8802ce15ead606ba9563f4a
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2014-12-07 11:35:09 +0000

    Encomendas inc

--- app/server/routes.js
+++ app/server/routes.js
@@ -30,7 +30,7 @@ exports.listen = function (app) {
     app.get('/orders', function (req, res) {
         var messages = generateMessageBlock();
         if (req.session.user) {
-            requestify.request('http://localhost:49445/api/encomendas?CodigoCliente='+req.session.user.CodigoCliente, {method: 'GET', dataType: 'form-url-encoded'})
+            requestify.request('http://localhost:49445/api/encomendas?CodigoCliente=CL000001'/*+req.session.user.CodigoCliente*/, {method: 'GET', dataType: 'form-url-encoded'})
                 .then(function (response) {
                     if (response.getCode() == "200") {
                         orders = response.getBody();
