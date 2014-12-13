18e5f9ff1fa583b33660f3e9446dbd323b3a396d
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2014-12-13 13:22:01 +0000

    Carrinho de compras a ser feito

--- app/server/routes.js
+++ app/server/routes.js
@@ -204,6 +204,7 @@ exports.listen = function (app) {
                     messages.success.push({title: "Sucesso", content: "Produto adicionado ao carrinho"});
                     var totalItems = 0;
                     console.log("antes");
+                    console.log(req.session.shoppingCart);
                     for (var i = 0; i < 0 < req.session.shoppingCart.length; i++) {
                         totalItems += req.session.shoppingCart[i]['quantidade'];
                     }
