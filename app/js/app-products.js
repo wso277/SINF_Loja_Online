4ac8e0a2c9a2eeb9f3aab0d69d70ff96bb654010
wso277 <oliveira.wilson93@gmail.com>
2014-12-13 14:37:57 +0000

    Adding pagination

--- app/js/app-products.js
+++ app/js/app-products.js
@@ -14,7 +14,8 @@
             $http.post("/get-page", {page: page})
                 .success(function (data) {
                     alert("success");
-                    data.foreach(function (product) {
+                    alert(data);
+                    data.produtos.foreach(function (product) {
                         angular.element('<article class="search-result row" style="background-color: #222222"> <div class="col-xs-12 col-sm-12 col-md-3"> <a href="/product/' + product.CodigoArtigo + '" class="thumbnail"><img src="http://lorempixel.com/400/200/" alt="Lorem ipsum"/></a> </div> <div class="col-xs-12 col-sm-12 col-md-2"> <ul class="meta-search"> <li><i class="fa fa-android"></i> <span>' + product.NomeSistemaOperativo + '</span> </li> <li><i class="fa fa-mobile"></i> <span>' + product.TamanhoEcra + ' polegadas</span> </li> <li><i class="fa fa-mobile"></i> <span>' + product.StockAtual + ' in stock</span> </li> <li id="preco"><i class="fa fa-euro"></i> <span>' + product.PVP * (1 - (product.Desconto / 100)) + '</span> </li> </ul> </div> <div class="col-xs-12 col-sm-12 col-md-7 excerpet"> <h3><a href="/product/' + product.CodigoArtigo + '" title="">' + product.Nome + '</a></h3> <p>Processador: ' + product.CPU + '</p> <p>Armazenamento: ' + product.Armazenamento + '</p> <p>Data de Lançamento: ' + product.Lancamento + '</p> </div> <span class="clearfix borda"></span> </article>')
                     });
                 })
