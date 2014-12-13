970c9e62db1608d24f07379258fdc92220f32a74
wso277 <oliveira.wilson93@gmail.com>
2014-12-13 14:50:28 +0000

    Adding pagination

--- app/js/app-products.js
+++ app/js/app-products.js
@@ -14,9 +14,9 @@
                 .success(function (data) {
                     console.log(data);
 
-                    data.produtos.foreach(function (product) {
-                        angular.element('<article class="search-result row" style="background-color: #222222"> <div class="col-xs-12 col-sm-12 col-md-3"> <a href="/product/' + product.CodigoArtigo + '" class="thumbnail"><img src="http://lorempixel.com/400/200/" alt="Lorem ipsum"/></a> </div> <div class="col-xs-12 col-sm-12 col-md-2"> <ul class="meta-search"> <li><i class="fa fa-android"></i> <span>' + product.NomeSistemaOperativo + '</span> </li> <li><i class="fa fa-mobile"></i> <span>' + product.TamanhoEcra + ' polegadas</span> </li> <li><i class="fa fa-mobile"></i> <span>' + product.StockAtual + ' in stock</span> </li> <li id="preco"><i class="fa fa-euro"></i> <span>' + product.PVP * (1 - (product.Desconto / 100)) + '</span> </li> </ul> </div> <div class="col-xs-12 col-sm-12 col-md-7 excerpet"> <h3><a href="/product/' + product.CodigoArtigo + '" title="">' + product.Nome + '</a></h3> <p>Processador: ' + product.CPU + '</p> <p>Armazenamento: ' + product.Armazenamento + '</p> <p>Data de Lançamento: ' + product.Lancamento + '</p> </div> <span class="clearfix borda"></span> </article>')
-                    });
+                    for (i = 0; i < data.length;i++) {
+                        angular.element('<article class="search-result row" style="background-color: #222222"> <div class="col-xs-12 col-sm-12 col-md-3"> <a href="/product/' + data[i].CodigoArtigo + '" class="thumbnail"><img src="http://lorempixel.com/400/200/" alt="Lorem ipsum"/></a> </div> <div class="col-xs-12 col-sm-12 col-md-2"> <ul class="meta-search"> <li><i class="fa fa-android"></i> <span>' + data[i].NomeSistemaOperativo + '</span> </li> <li><i class="fa fa-mobile"></i> <span>' + data[i].TamanhoEcra + ' polegadas</span> </li> <li><i class="fa fa-mobile"></i> <span>' + data[i].StockAtual + ' in stock</span> </li> <li id="preco"><i class="fa fa-euro"></i> <span>' + data[i].PVP * (1 - (data[i].Desconto / 100)) + '</span> </li> </ul> </div> <div class="col-xs-12 col-sm-12 col-md-7 excerpet"> <h3><a href="/product/' + data[i].CodigoArtigo + '" title="">' + data[i].Nome + '</a></h3> <p>Processador: ' + data[i].CPU + '</p> <p>Armazenamento: ' + data[i].Armazenamento + '</p> <p>Data de Lançamento: ' + data[i].Lancamento + '</p> </div> <span class="clearfix borda"></span> </article>')
+                    };
                 })
         };
 
